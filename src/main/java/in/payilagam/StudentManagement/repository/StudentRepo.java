05e25e180ffed2a9021e8aa0342074e11298b9fa
SnekaSB <snekasri09@gmail.com>
2023-10-08 20:46:19 +0530

    commit project

---
+++ src/main/java/in/payilagam/StudentManagement/repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package in.payilagam.StudentManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.payilagam.StudentManagement.entity.StudentEntity;
+
+public interface StudentRepo extends 
+	JpaRepository<StudentEntity, Long>{
+
+}
